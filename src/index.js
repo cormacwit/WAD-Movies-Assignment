07f5ec17cb00687796d9ad2850764e3d6280de03
cormacwit <cormackfarrell@gmail.com>
2022-12-13 00:28:17 +0000

    Added Upcoming movies page.

--- src/index.js
+++ src/index.js
@@ -6,21 +6,23 @@ import MoviePage from "./pages/movieDetailsPage";
 import FavouriteMoviesPage from "./pages/favouriteMoviesPage"; // NEW
 import MovieReviewPage from "./pages/movieReviewPage";
 import SiteHeader from './components/siteHeader';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
 import { Home } from "@mui/icons-material";
+import UpcomingPage from './pages/upcomingMoviesPage';
 
 const App = () => {
   return (
       <BrowserRouter>
           <SiteHeader />      {FavouriteMoviesPage}
           <SiteHeader />      {HomePage}
-          
+
       <Routes>
         <Route exact path="/movies/favourites" element={<FavouriteMoviesPage />} />
         <Route path="/movies/:id" element={<MoviePage />} />
         <Route path="/" element={<HomePage />} />
         <Route path="/reviews/:id" element={ <MovieReviewPage /> } />
         <Route path="*" element={ <Navigate to="/" /> } />
+        <Route path="/movies/upcoming" element={ <UpcomingPage /> } />
       </Routes>
     </BrowserRouter>
   );
